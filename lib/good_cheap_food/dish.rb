97af4c35dfe140d5b56b55ce21e77dfdce104cb7
ryan mcmahon <ryan@mcmahon.work>
2017-03-28 15:02:23 -0400

    added dish class

--- lib/good_cheap_food/dish.rb
+++ lib/good_cheap_food/dish.rb
@@ -1,5 +1,16 @@
 class Dish
-  attr_accessor :name, :cuisine, :restaurants
+  attr_accessor :name, :price, :cuisine, :restaurants
 
-  
+  def initialize(name, price)
+    @name = name
+    price = price
+    @restaurants = []
+  end
+
+  def cuisine(cuisine)
+    @cuisine = cuisine
+  end
+  #cuisine through restaurants?
+
+
 end
