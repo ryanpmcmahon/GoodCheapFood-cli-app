c6a90f76773dcee51efd789e5f2541c636e711dc
ryan mcmahon <ryan@mcmahon.work>
2017-04-06 15:00:57 -0400

    refractor

--- lib/concerns/creatable.rb
+++ lib/concerns/creatable.rb
@@ -1,27 +1,19 @@
 module Creatable
-  module Instancethods
+  include Findable
 
-  end
-
-  module ClassMethods
-    def create(name)
-      new.tap do |x|
-        x.name = name
-        x.save
-      end
-    end
-
-    def find_by_name(name)
-      obj = all.select{|x| x.name = name}
-      obj[0]
-    end
-
-    def find_or_create_by_name(name)
-      if find_by_name(name)
-        find_by_name(name)
-      else
-        create(name)
-      end
+  def create(name)
+    new.tap do |x|
+      x.name = name
+      x.save
     end
   end
+
+  def find_or_create_by_name(name)
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      create(name)
+    end
+  end
+
 end
--- lib/good_cheap_food/dish.rb
+++ lib/good_cheap_food/dish.rb
@@ -1,4 +1,9 @@
 class Dish
+  include Createable
+  include Findable
+  include Savable::ClassMethods
+  extend Savable::InstanceMethods
+
   attr_accessor :name, :price, :cuisine, :restaurants
 
   @@all = []
