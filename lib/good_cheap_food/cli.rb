ba88e57feabc1a649e7713479e45eed99e0b55fb
ryan mcmahon <ryan@mcmahon.work>
2017-04-13 15:26:39 -0400

    start of cli class

---
+++ lib/good_cheap_food/cli.rb
@@ -0,0 +1,11 @@
+
+class CLI
+
+  def call
+    puts "Welcome to Good, Cheap Food! Here's how to start:"
+    puts "1. Search for food or restaurants by neighborhood, cuisine, or name."
+    puts "2. Or, see a list by typing 'List [neighborhoods, cuisines, dishes, restaurants]'"
+  end
+
+
+end
