00a589a398cb4da8141189c1f721a4f7da477f7b
ryan mcmahon <ryan@mcmahon.work>
2017-04-14 11:29:09 -0400

    added notes on CLI requirements

--- lib/good_cheap_food/cli.rb
+++ lib/good_cheap_food/cli.rb
@@ -9,3 +9,12 @@ class CLI
 
 
 end
+
+
+### Basic functionality ###
+# - Search by neighorhood ==> Restaurant, Cuisine ==> Rest. drill down: Rest, address, phone, dishes
+# - Search by cuisine ==> Restaurant, Neighborhood ==> Rest. drill down: Rest, address, phone, dishes
+# - Search by dish ==> Price, @ Restaurant ==> Rest. drill down: Rest, address, phone, dishes
+# - List all restaurants ==> Restaurant, neighborhood, cuisine
+# - List all cuisines ==> Cuisines, in neighborhoods ==> Cuisine: Rest, Neighborhood ==> Rest. drill down: Rest, address, phone, dishes
+# - List all neighborhoods ==> Neighborhoods (with cuisines?)
